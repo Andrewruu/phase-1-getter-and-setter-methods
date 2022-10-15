e4a3b205af6f33ef767d0400d00b2fb0cb5d217a
Andrew Wang <Andrewruujin@gmail.com>
2022-10-15 13:43:35 -0400

    commit tree 59306bcea2a6040695cfb543733fbd11001b3fc6

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your Circle class here
+
+class Circle{
+
+    constructor(radius){
+        this.radius = radius;
+    }
+    get diameter(){
+        return 2*this.radius
+    }
+    get circumference(){
+        return Math.PI*this.diameter
+    }
+    get area(){
+        return Math.PI*this.radius*this.radius
+    }
+
+    set diameter(diameter){
+        this.radius = diameter/2
+    }
+    set circumference(circumference){
+        this.radius = circumference/Math.PI/2
+    }
+    set area(area){
+        this.radius = Math.sqrt(area/Math.PI)
+    }
+}
+
+const x = new Circle(3)
+console.log(x.diameter)
+console.log(x.circumference)
+console.log(x.area)
+x.diameter = 8
+console.log(x.radius)
+x.circumference = 8
+console.log(x.radius)
+x.area = 8
+console.log(x.radius)
